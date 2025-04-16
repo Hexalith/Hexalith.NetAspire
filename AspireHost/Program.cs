c427953969599b967c8a2fc2f347b985d374f781
Jérôme Piquot <jpiquot@itaneo.com>
2025-04-16 09:51:24 +0200

    Initial commit

---
+++ AspireHost/Program.cs
@@ -0,0 +1,8 @@
+// <copyright file="Program.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+await builder.Build().RunAsync();
